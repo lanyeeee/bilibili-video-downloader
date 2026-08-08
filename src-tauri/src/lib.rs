ace6b8efa0d9f6f7af719efd04fef509a473b699
lanyeeee <lanyeeeeee@gmail.com>
2026-08-09 04:03:41 +0800

    feat: 支持窗口状态记忆

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -106,6 +106,7 @@ pub fn run() {
     }
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_opener::init())
